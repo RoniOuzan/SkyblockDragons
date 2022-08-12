182f138f5704b528ad2a3760b0bac328a86eccd8
BuildTools <unconfigured@null.spigotmc.org>
2022-08-12 18:40:08 +0300

    Added Scylla Added Hyperion Added Necorn Blade Added Astraea

--- src/main/java/me/maxiiiiii/skyblockdragons/item/material/types/NecronBladeMaterial.java
+++ src/main/java/me/maxiiiiii/skyblockdragons/item/material/types/NecronBladeMaterial.java
@@ -20,7 +20,7 @@ public class NecronBladeMaterial extends SwordMaterial {
     }
 
     public enum NecronBladeType {
-        VALKYRIE, HYPERION, SCYLLA, ASTRAEA
+        VALKYRIE, HYPERION, SCYLLA, ASTRAEA, NECRON_BLADE
     }
 
     @Getter
