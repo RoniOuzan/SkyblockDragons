2cea3fe471fdadaa84fcd447d2611468f6b43e95
Yoav <yoaverror@gmail.com>
2022-10-13 16:50:50 +0300

    PlayerPlaceFurEvent.java deleted EntityBear Added

---
+++ src/main/java/me/maxiiiiii/skyblockdragons/entity/types/bearisland/EntityBear.java
@@ -0,0 +1,56 @@
+package me.maxiiiiii.skyblockdragons.entity.types.bearisland;
+
+import me.maxiiiiii.skyblockdragons.entity.EntityMaterial;
+import me.maxiiiiii.skyblockdragons.entity.EntitySD;
+import me.maxiiiiii.skyblockdragons.item.material.types.ItemMaterial;
+import me.maxiiiiii.skyblockdragons.util.objects.Equipment;
+import me.maxiiiiii.skyblockdragons.util.objects.FlyToLocation;
+import me.maxiiiiii.skyblockdragons.worlds.bearisland.BearIsland;
+import me.maxiiiiii.skyblockdragons.worlds.witherisland.WitherIsland;
+import org.bukkit.Location;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Wither;
+
+import java.util.UUID;
+
+public abstract class EntityBear extends EntityMaterial {
+    public static final int TICKS_TO_SECONDS = 20;
+    public int moveRate = 80;
+    public int phase = 0;
+    public int i = 0;
+    public UUID uuid = null;
+    public EntitySD entitySD;
+    public FlyToLocation flyToLocation;
+    public String color = "§8";
+    public ItemMaterial crystal = null;
+    public int tickRate = 1;
+
+    public Location middleLoc;
+
+    public EntityBear(String name, double health, double damage, double trueDamage, int moveRate, String color, int tickRate) {
+        super(EntityType.WITHER, name, -1, health, 100, damage, trueDamage, new Equipment(), 100, 1, true, 0, 0);
+        this.moveRate = moveRate;
+        this.color = color;
+        this.tickRate = tickRate;
+    }
+
+    public EntityBear(String name, int level, double health, double defense, double damage, double trueDamage, Equipment equipment, double speed) {
+        this(name, level, health, defense, damage, trueDamage, equipment, speed, 1);
+
+    }
+
+    public EntityBear(String name, int level, double health, double defense, double damage, double trueDamage, Equipment equipment, double speed, double knockbackResistance) {
+        super(EntityType.IRON_GOLEM, name, level, health, defense, damage, trueDamage, equipment, speed, knockbackResistance, true, 0, 0);
+    }
+
+    @Override
+    public void onSpawn(EntitySD entity) {
+        if (entity.entity instanceof Wither){
+            BearIsland.iron_golem = entity;
+            BearIsland.bearDamage.clear();
+            uuid = entity.entity.getUniqueId();
+            entitySD = entity;
+        }
+    }
+}
--- src/main/java/me/maxiiiiii/skyblockdragons/worlds/bearisland/BearIsland.java
+++ src/main/java/me/maxiiiiii/skyblockdragons/worlds/bearisland/BearIsland.java
@@ -1,179 +1,199 @@
 package me.maxiiiiii.skyblockdragons.worlds.bearisland;
 
-import de.tr7zw.changeme.nbtapi.NBTEntity;
 import me.maxiiiiii.skyblockdragons.SkyblockDragons;
 import me.maxiiiiii.skyblockdragons.entity.EntityMaterial;
 import me.maxiiiiii.skyblockdragons.entity.EntitySD;
+import me.maxiiiiii.skyblockdragons.entity.types.bearisland.EntityBear;
+import me.maxiiiiii.skyblockdragons.item.material.Items;
+import me.maxiiiiii.skyblockdragons.item.material.types.ItemMaterial;
 import me.maxiiiiii.skyblockdragons.player.PlayerSD;
 import me.maxiiiiii.skyblockdragons.util.Functions;
+import me.maxiiiiii.skyblockdragons.util.objects.Cuboid;
 import me.maxiiiiii.skyblockdragons.util.objects.Killer;
-
 import me.maxiiiiii.skyblockdragons.world.WorldSD;
 import me.maxiiiiii.skyblockdragons.world.WorldType;
 import me.maxiiiiii.skyblockdragons.world.warp.Warp;
-import me.maxiiiiii.skyblockdragons.worlds.bearisland.events.BearKillEvent;
-import me.maxiiiiii.skyblockdragons.worlds.bearisland.events.PlayerPlaceFurEvent;
-
 import org.bukkit.*;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.Vector;
-
+import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class BearIsland extends WorldSD implements Listener {
     public static final World world = Bukkit.getWorld("BearIsland");
-    public static final Location MIDDLE_OF_LOOT = new Location(world, -59.486, 90.00000, 197.481);
-    public static final Location BEAR_SPAWN = new Location(world, -62.500, 92.00000, 212.500);
-    public static final Location MIDDLE = new Location(world, -62.500, 92.00000, 212.500);
-    public static EntitySD bear = null;
+    public static final Location BEAR_SPAWN = new Location(world, -62.599, 92.06250, 212.430);
+    public static final int TIME_FOR_BEAR_SOUL = 60;
+    public static EntitySD iron_golem = null;
 
-    public static Map<UUID, Double> bearDamage = new HashMap<>();
+    public static final Map<UUID, Double> bearDamage = new HashMap<>();
+    public static final Map<UUID, Integer> amountOfPlacedEyes = new HashMap<>();
 
     public BearIsland(JavaPlugin plugin) {
         super(world, "Bear Island", Warp.BEAR_ISLAND, WorldType.COMBAT);
-        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        clearBearArea();
+        buildAllBearSoul();
+    }
+
+    @EventHandler
+    public void onBearDeath(EntityDeathEvent e) {
+        try {
+            LivingEntity entity = e.getEntity();
+            if (iron_golem.getUniqueId() != null && !iron_golem.getUniqueId().equals(iron_golem.getUniqueId())) {
+                Functions.Wait(20, () -> sendBearDeadMessage(e.getEntity().getKiller()));
+            }
+        } catch (NullPointerException ignored) {
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Block block = event.getBlock();
+        PlayerSD player = SkyblockDragons.getPlayer(event.getPlayer());
+        if (block.getWorld() == world) {
+            ItemStack item = event.getItemInHand();
+            ItemMaterial material = Functions.getItemMaterial(item);
+            if (material == Items.get("BEAR_EYE")) {
+                if (block.getY() == 73 && block.getZ() == 63) {
+                    if (block.getX() <= -63 && block.getX() >= -65) {
+                        onBearEyePlace(event);
+                    }
+                }
+            }
+        }
     }
 
     public static EntityMaterial getRandomBear() {
         double random = Math.random() * 100;
-        if (random >= 84)
-            return EntityMaterial.get("GRIZZLY_BEAR");
         if (random >= 68)
-            return EntityMaterial.get("POLAR_BEAR");
-        if (random >= 52)
-            return EntityMaterial.get("PANDA_BEAR");
-        if (random >= 36)
             return EntityMaterial.get("KOALA_BEAR");
+        if (random >= 52)
+            return EntityMaterial.get("POLAR_BEAR");
+        if (random >= 36)
+            return EntityMaterial.get("PANDA_BEAR");
+        if (random >= 4)
+            return EntityMaterial.get("GRIZZLY_BEAR");
         if (random >= 1)
             return EntityMaterial.get("RED_PANDA_BEAR");
-        return EntityMaterial.get("RED_PANDA_BEAR");
+        return EntityMaterial.get("GRIZZLY_BEAR");
     }
 
-    public static void spawnBear() {
-        bear = new EntitySD(BEAR_SPAWN, getRandomBear());
-        NBTEntity nbtEntity = new NBTEntity(bear.entity);
-        nbtEntity.setInteger("BearPhase", 1);
+    public int getAmountOfEyes() {
+        return amountOfPlacedEyes.values().stream().mapToInt(Integer::intValue).sum();
+    }
+
+    public void onBearEyePlace(BlockPlaceEvent event) {
+        Block block = event.getBlock();
+        PlayerSD player = SkyblockDragons.getPlayer(event.getPlayer());
+        UUID uuid = player.getUniqueId();
+        event.setCancelled(false);
+        Integer eyes = amountOfPlacedEyes.getOrDefault(uuid, 0);
+        amountOfPlacedEyes.put(uuid, eyes + 1);
+        for (Player loop_player : player.getWorld().getPlayers()) {
+            loop_player.sendMessage(ChatColor.DARK_PURPLE + "☬ " + player.getDisplayName() + ChatColor.LIGHT_PURPLE + " placed a eye-! (" + getAmountOfEyes() + "/3)");
+        }
+
+        if (getAmountOfEyes() >= 3) {
+            this.spawnBear();
+        }
+    }
+
+    public void spawnBear() {
+        iron_golem = new EntitySD(BEAR_SPAWN, getRandomBear());
+        clearBearArea();
+    }
+
+    public void clearBearArea() {
+        Cuboid cuboid = new Cuboid(new Location(world, -65, 71, 63), new Location(world, -63, 73, 63));
+        cuboid.forEach(block -> {
+            block.setType(Material.AIR);
+        });
+    }
+
+    public void buildAllBearSoul() {
+        for (int i = 1; i <= 4; i++) {
+            buildConcrete(i);
+        }
+    }
+
+    public void buildConcrete(int num) {
+        Location location = getConcrete(num);
+        location.getBlock().setType(Material.CONCRETE);
+    }
+
+    public Location getConcrete(int num) {
+        switch (num) {
+            case 1:
+                return new Location(world, -62, 92, 212);
+            case 2:
+                return new Location(world, -62, 93, 212); // right side of the concrete
+            case 3:
+                return new Location(world, -62.500, 94.00000, 212.500); // niddle side of the concrete
+            case 4:
+                return new Location(world, -62, 94, 212); // the left side of the concrete
+            default:
+                return new Location(world, -62, 92, 212);
+        }
+    }
+
+
+    public void sendBearDeadMessage(Player lastDamager) {
+        EntityBear type = (EntityBear) iron_golem.type;
+        Map<UUID, Double> sortedBearDamageMap = sortedBearDamageMap();
+        List<UUID> damageDealers = sortedBearDamage();
+        List<Killer> killers = new ArrayList<>();
+        amountOfPlacedEyes.clear();
+        for (UUID killer : damageDealers) {
+            killers.add(new Killer(SkyblockDragons.getPlayer(killer), sortedBearDamageMap.get(killer)));
+        }
+        Collections.reverse(killers);
         for (Player player : Bukkit.getOnlinePlayers().stream().filter(p -> p.getWorld().getName().equals("BearIsland")).collect(Collectors.toList())) {
-            if (player.getLocation().distance(MIDDLE) <= 50) {
-                player.setVelocity(new Vector(
-                        BEAR_SPAWN.getX() - player.getLocation().getX(),
-                        0,
-                        BEAR_SPAWN.getZ() - player.getLocation().getZ()
-                ).normalize().multiply(2).setY(3));
-            }
-        }
-    }
-    public static final Map<PlayerSD, Integer> amountOfPlacedFurs = new HashMap<>();
+            player.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "----------------------------------------");
+            player.sendMessage("                      " + type.color + "" + ChatColor.BOLD + Functions.setTitleCase(type.name) + ChatColor.GOLD + ChatColor.BOLD + " DOWN!");
+            player.sendMessage(ChatColor.RESET + " ");
+            if (lastDamager != null)
+                player.sendMessage("           " + ChatColor.WHITE + lastDamager.getDisplayName() + ChatColor.GRAY + " dealt the final hit!");
+            player.sendMessage(ChatColor.RESET + " ");
+            if (killers.size() > 0)
+                player.sendMessage("           " + ChatColor.YELLOW + "" + ChatColor.BOLD + "1st Damager " + ChatColor.GRAY + "- " + killers.get(0).player.getDisplayName() + " " + ChatColor.GRAY + "- " + ChatColor.YELLOW + Functions.getNumberFormat(killers.get(0).damage));
+            if (killers.size() > 1)
+                player.sendMessage("         " + ChatColor.GOLD + "" + ChatColor.BOLD + "2nd Damager " + ChatColor.GRAY + "- " + killers.get(1).player.getDisplayName() + " " + ChatColor.GRAY + "- " + ChatColor.YELLOW + Functions.getNumberFormat(killers.get(1).damage));
+            if (killers.size() > 2)
+                player.sendMessage("       " + ChatColor.RED + "" + ChatColor.BOLD + "3rd Damager " + ChatColor.GRAY + "- " + killers.get(2).player.getDisplayName() + " " + ChatColor.GRAY + "- " + ChatColor.YELLOW + Functions.getNumberFormat(killers.get(2).damage));
+            player.sendMessage(ChatColor.RESET + " ");
 
-    public void onPlaceFur(PlayerPlaceFurEvent e) {
-        e.getBlock().setData((byte) (e.getBlock().getData() + 4));
-        e.addToAmountOfFurs();
-        amountOfPlacedFurs.put(e.getPlayer(), amountOfPlacedFurs.getOrDefault(e.getPlayer(), 0) + 1);
-        for (Player player : Bukkit.getOnlinePlayers().stream().filter(p -> p.getWorld().getName().equals("BearIsland")).collect(Collectors.toList())) {
-            player.sendMessage( ChatColor.DARK_PURPLE + "☬ " + e.getPlayer().getDisplayName() + ChatColor.LIGHT_PURPLE + " placed an fur! (" + e.getAmountOfFurs() + "/8)");
-        }
-
-
-        if (e.getAmountOfFurs() >= 8) {
-            Functions.Wait(20L, BearIsland::spawnBear);
-        }
-    }
-    @EventHandler
-    public void onClickBlock(PlayerInteractEvent e) {
-        if (e.getClickedBlock() == null) return;
-        if (e.getAction() != Action.RIGHT_CLICK_BLOCK && e.getAction() != Action.LEFT_CLICK_BLOCK) return;
-
-        if (e.getClickedBlock().getType() == Material.ENDER_PORTAL_FRAME && e.getClickedBlock().getData() < 4) {
-            ItemStack item = e.getItem();
-            if (Functions.getId(item).equals("BEAR_EYE")) {
-                if (e.getClickedBlock().getLocation().distance(MIDDLE) <= 5) {
-                    if (e.getPlayer().getGameMode() != GameMode.CREATIVE)
-                        e.getPlayer().getEquipment().setItemInMainHand(null);
-                    e.setCancelled(true);
-                    PlayerPlaceFurEvent event = new PlayerPlaceFurEvent(SkyblockDragons.getPlayer(e.getPlayer()), e.getClickedBlock(), e.getItem());
-                    Bukkit.getServer().getPluginManager().callEvent(event);
-                    onPlaceFur(event);
-                }
-            }
-        }
-    }
-
-    public static void resetFurs() {
-        for (Block block : Functions.loopBlocksHorizontally(MIDDLE, 10)) {
-            if (block.getType() == Material.ENDER_PORTAL_FRAME && block.getData() > 3) {
-                block.setData((byte) (block.getData() - 4));
-            }
-        }
-    }
-
-
-        @EventHandler
-        public void onBearKill(BearKillEvent e){
-            List<Killer> killers = new ArrayList<>();
-            for (PlayerSD player : e.getKillers().keySet()) {
-                killers.add(new Killer(player, e.getKillers().get(player)));
-            }
-            Collections.sort(killers);
-
-            BearType bearType = BearType.getBearType(e.getEntity().type.getName());
-
-            if (bearType == null) {
-                SkyblockDragons.logger.info(ChatColor.RED + "Cannot find the type of the bear!");
-                return;
-            }
-
-            Map<PlayerSD, Integer> positions = new HashMap<>();
-            // 1 to make the place be easier to see
-            for (int i = 1; i < killers.size() + 1; i++) {
-                PlayerSD player = killers.get(i - 1).player;
-                double damage = killers.get(i - 1).damage;
-
-                positions.put(player, i);
-            }
-
-            for (Player player : Bukkit.getOnlinePlayers().stream().filter(p -> p.getWorld().getName().equals("BearIsland")).collect(Collectors.toList())) {
-                player.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "----------------------------------------");
-                player.sendMessage("                      " + bearType.color + "" + ChatColor.BOLD + Functions.setTitleCase(bearType.name()) + " Bear");
+            if (sortedBearDamageMap.containsKey(player.getUniqueId())) {
+                player.sendMessage("                  " + ChatColor.YELLOW + "Your Damage: " + ChatColor.GREEN + Functions.getNumberFormat(sortedBearDamageMap.get(player.getUniqueId())) + " " + ChatColor.GRAY + "(Position #" + (damageDealers.size() - damageDealers.indexOf(player.getUniqueId())) + ")");
                 player.sendMessage(ChatColor.RESET + " ");
-                player.sendMessage("           " + ChatColor.WHITE + e.getKiller().getDisplayName() + ChatColor.GRAY + " dealt the final hit!");
-                player.sendMessage(ChatColor.RESET + " ");
-                if (killers.size() > 0)
-                    player.sendMessage("           " + ChatColor.YELLOW + "" + ChatColor.BOLD + "1st Damager " + ChatColor.GRAY + "- " + killers.get(0).player.getDisplayName() + " " + ChatColor.GRAY + "- " + ChatColor.YELLOW + Functions.getNumberFormat(killers.get(0).damage));
-                if (killers.size() > 1)
-                    player.sendMessage("         " + ChatColor.GOLD + "" + ChatColor.BOLD + "2nd Damager " + ChatColor.GRAY + "- " + killers.get(1).player.getDisplayName() + " " + ChatColor.GRAY + "- " + ChatColor.YELLOW + Functions.getNumberFormat(killers.get(1).damage));
-                if (killers.size() > 2)
-                    player.sendMessage("       " + ChatColor.RED + "" + ChatColor.BOLD + "3rd Damager " + ChatColor.GRAY + "- " + killers.get(2).player.getDisplayName() + " " + ChatColor.GRAY + "- " + ChatColor.YELLOW + Functions.getNumberFormat(killers.get(2).damage));
-                player.sendMessage(ChatColor.RESET + " ");
-                if (e.getKillers().containsKey(SkyblockDragons.getPlayer(player))) {
-                    player.sendMessage("                  " + ChatColor.YELLOW + "Your Damage: " + ChatColor.GREEN + Functions.getNumberFormat(e.getKillers().get(SkyblockDragons.getPlayer(player))) + " " + ChatColor.GRAY + "(Position #" + (positions.get(SkyblockDragons.getPlayer(player))) + ")");
-                    player.sendMessage(ChatColor.RESET + " ");
-                }
-                player.sendMessage(ChatColor.RESET + "" + ChatColor.GREEN + "" + ChatColor.BOLD + "----------------------------------------");
             }
-
-            BearIsland.bearDamage.clear();
-            BearIsland.bear = null;
-            amountOfPlacedFurs.clear();
-            PlayerPlaceFurEvent.resetAmountOfFurs();
-
-            Functions.Wait(100L, BearIsland::resetFurs);
-
-            for (Block block : Functions.loopBlocksHorizontally(BearIsland.MIDDLE_OF_LOOT, 5.5)) {
-                block.setType(Material.DIRT);
-            }
-            Functions.Wait(400L, () -> {
-                for (Block block : Functions.loopBlocksHorizontally(BearIsland.MIDDLE_OF_LOOT, 5.5)) {
-                    block.setType(Material.GRASS);
-                }
-            });
+            player.sendMessage(ChatColor.RESET + "" + ChatColor.GREEN + "" + ChatColor.BOLD + "----------------------------------------");
         }
+        iron_golem = null;
+        buildBlackConcrteSlowly();
+
     }
+
+    public void buildBlackConcrteSlowly() {
+        Functions.Loop(4, 20 * TIME_FOR_BEAR_SOUL, amount -> {
+            buildConcrete(amount + 1);
+        });
+    }
+
+    public static List<UUID> sortedBearDamage() {
+        return new ArrayList<>(sortedBearDamageMap().keySet());
+    }
+
+    @NotNull
+    public static Map<UUID, Double> sortedBearDamageMap() {
+        return Functions.sortByValue(bearDamage);
+    }
+}
+
