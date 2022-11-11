95e68dc28d75302caf5492c77e6b7aac4a4d1393
RoniOuzan <roniouzan@gmail.com>
2022-11-11 12:43:09 +0200

    Fixed the lore of crystals

--- src/main/java/me/maxiiiiii/skyblockdragons/item/material/types/CrystalMaterial.java
+++ src/main/java/me/maxiiiiii/skyblockdragons/item/material/types/CrystalMaterial.java
@@ -5,6 +5,7 @@ import me.maxiiiiii.skyblockdragons.item.crystals.Crystal;
 import me.maxiiiiii.skyblockdragons.item.objects.ItemFamily;
 import me.maxiiiiii.skyblockdragons.item.objects.ItemType;
 import me.maxiiiiii.skyblockdragons.item.objects.Rarity;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 
 @Getter
@@ -18,7 +19,7 @@ public class CrystalMaterial extends ToolMaterial {
                 crystal.getCrystal().getName() + " Crystal Level " + crystal.getLevel(),
                 ItemType.CRYSTAL,
                 Rarity.getRarity(crystal.getLevel()),
-                p -> "Apply on items in the Crystal Grinder to add additional stats. Adds " + crystal.getStat().getStatDisplay() + " to your stats."
+                p -> "Apply on items in the Crystal NEW_LINE Grinder to add additional stats. Adds " + crystal.getStat().getStatDisplay() + ChatColor.GRAY + " to your stats."
         );
         this.crystal = crystal;
     }
